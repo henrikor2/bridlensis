826d07ee2db003b67503d83a849f3310b86c610c
Henri Kor <henrikor2@gmail.com>
2014-06-15 23:03:38 -0400

    Move raw Markdown files to source folders; Simplify HTMLConvert.java

--- src/doc/bridlensis/doc/HTMLConvert.java
+++ src/doc/bridlensis/doc/HTMLConvert.java
@@ -1,8 +1,6 @@
 package bridlensis.doc;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -19,18 +17,18 @@ public class HTMLConvert {
 
 	private static final String UTF_8 = "UTF-8";
 
-	private static final String MANUAL_INTRO = "manual_intro.md";
-	private static final String MANUAL_USAGE = "manual_usage.md";
-	private static final String MANUAL_LANGREF = "manual_langref.md";
-	private static final String MANUAL_FUNCREF = "manual_funcref.md";
-	private static final String MANUAL_FUNCREF_INSTRUCTIONS = "manual_funcref_instructions.md";
-	private static final String MANUAL_FUNCREF_HEADERS = "manual_funcref_headers.md";
+	private static final String MANUAL_MD_INTRO = "manual_intro.md";
+	private static final String MANUAL_MD_USAGE = "manual_usage.md";
+	private static final String MANUAL_MD_LANGREF = "manual_langref.md";
+	private static final String MANUAL_MD_FUNCREF = "manual_funcref.md";
+	private static final String MANUAL_MD_FUNCREF_INSTRUCTIONS = "manual_funcref_instructions.md";
+	private static final String MANUAL_MD_FUNCREF_HEADERS = "manual_funcref_headers.md";
 	private static final String MANUAL_CSS = "manual.css";
-	private static final String MANUAL_HTML = "Manual.html";
+	private static final String MANUAL_MD_HTML_FILENAME = "Manual.html";
 
 	private static final String RELNOTES_MD = "release_notes.md";
-	private static final String RELNOTES_CSS = "manual.css";
-	private static final String RELNOTES_HTML = "Release Notes.html";
+	private static final String RELNOTES_CSS = MANUAL_CSS;
+	private static final String RELNOTES_HTML_FILENAME = "Release Notes.html";
 
 	public static void main(String[] args) {
 		int exitCode = 0;
@@ -45,8 +43,8 @@ public class HTMLConvert {
 	}
 
 	private static void writeReleaseNotes() throws IOException {
-		try (BufferedWriter output = beginHTMLFile(new File(RELNOTES_HTML),
-				new Scanner(new File(RELNOTES_CSS), UTF_8))) {
+		try (BufferedWriter output = beginHTMLFile(RELNOTES_CSS,
+				RELNOTES_HTML_FILENAME)) {
 			markdownToHtml(RELNOTES_MD, output);
 			endHTMLFile(output);
 		}
@@ -54,25 +52,25 @@ public class HTMLConvert {
 	}
 
 	private static void writeManual() throws IOException {
-		try (BufferedWriter output = beginHTMLFile(new File(MANUAL_HTML),
-				new Scanner(new File(MANUAL_CSS), UTF_8))) {
-			markdownToHtml(MANUAL_INTRO, output);
-			markdownToHtml(MANUAL_USAGE, output);
-			markdownToHtml(MANUAL_LANGREF, output);
-			markdownToHtml(MANUAL_FUNCREF, output);
-			markdownToHtml(MANUAL_FUNCREF_HEADERS, output);
-			writeFunctionsReferences(
+		try (BufferedWriter output = beginHTMLFile(MANUAL_CSS,
+				MANUAL_MD_HTML_FILENAME)) {
+			markdownToHtml(MANUAL_MD_INTRO, output);
+			markdownToHtml(MANUAL_MD_USAGE, output);
+			markdownToHtml(MANUAL_MD_LANGREF, output);
+			markdownToHtml(MANUAL_MD_FUNCREF, output);
+			markdownToHtml(MANUAL_MD_FUNCREF_HEADERS, output);
+			writeFunctionReference(
 					BuiltinElements.getBuiltinHeaderFunctionsDef(), output);
-			markdownToHtml(MANUAL_FUNCREF_INSTRUCTIONS, output);
-			writeFunctionsReferences(
-					BuiltinElements.getBuiltinInstructionsDef(), output);
+			markdownToHtml(MANUAL_MD_FUNCREF_INSTRUCTIONS, output);
+			writeFunctionReference(BuiltinElements.getBuiltinInstructionsDef(),
+					output);
 			output.write("<p style=\"color: #CCCCCC; margin-top: 24px;\">;eof BridleNSIS Manual</p>");
 			endHTMLFile(output);
 		}
 		System.out.println("Manual done");
 	}
 
-	private static void writeFunctionsReferences(InputStream input,
+	private static void writeFunctionReference(InputStream input,
 			BufferedWriter output) throws IOException {
 		try (Scanner scanner = new Scanner(input)) {
 			while (scanner.hasNextLine()) {
@@ -105,17 +103,16 @@ public class HTMLConvert {
 	private static void markdownToHtml(String markdownFile,
 			BufferedWriter output) throws IOException,
 			UnsupportedEncodingException, FileNotFoundException, AssertionError {
-		try (InputStreamReader inputUTF8 = getInputUTF8(markdownFile)) {
-			output.write(new Markdown4jProcessor().process(inputUTF8));
+		try (InputStreamReader input = openMarkdownSourceFile(markdownFile)) {
+			output.write(new Markdown4jProcessor().process(input));
 		}
 	}
 
-	private static InputStreamReader getInputUTF8(String fileName)
+	private static InputStreamReader openMarkdownSourceFile(String fileName)
 			throws UnsupportedEncodingException, FileNotFoundException,
 			IOException, AssertionError {
-		InputStreamReader input;
-		input = new InputStreamReader(new FileInputStream(new File(fileName)),
-				UTF_8);
+		InputStreamReader input = new InputStreamReader(
+				HTMLConvert.class.getResourceAsStream(fileName), UTF_8);
 		if (input.read() != 0xFEFF) {
 			throw new java.lang.AssertionError(fileName
 					+ " must be UTF-8 with BOM.");
@@ -129,23 +126,22 @@ public class HTMLConvert {
 		writer.close();
 	}
 
-	private static BufferedWriter beginHTMLFile(File htmlFile,
-			Scanner cssFileScanner) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(htmlFile));
-
+	private static BufferedWriter beginHTMLFile(String cssFileName,
+			String outputFileName) throws IOException {
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				outputFileName));
 		writer.write("<html>\r\n");
 		writer.write("<head>\r\n");
 		writer.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\r\n");
 		writer.write("</head>\r\n");
-
-		while (cssFileScanner.hasNextLine()) {
-			writer.write(cssFileScanner.nextLine());
-			writer.write("\r\n");
+		try (Scanner cssFileScanner = new Scanner(
+				HTMLConvert.class.getResourceAsStream(cssFileName), UTF_8)) {
+			while (cssFileScanner.hasNextLine()) {
+				writer.write(cssFileScanner.nextLine());
+				writer.write("\r\n");
+			}
 		}
-		cssFileScanner.close();
-
 		writer.write("<body>\r\n");
 		return writer;
 	}
-
 }
