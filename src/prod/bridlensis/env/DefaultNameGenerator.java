0df9d10f20b9a7e628e68fa22093191dfcb08970
Henri Kor <henrikor2@gmail.com>
2014-06-22 21:32:43 -0400

    Change DefaultNameGenerator to generate naturally increasing numbers instead of hash values

--- src/prod/bridlensis/env/DefaultNameGenerator.java
+++ src/prod/bridlensis/env/DefaultNameGenerator.java
@@ -1,32 +1,24 @@
 package bridlensis.env;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 public class DefaultNameGenerator implements NameGenerator {
 
-	private MessageDigest md;
+	private char prefix;
+	private short count;
 
 	public DefaultNameGenerator() {
-		try {
-			md = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e) {
-			throw new RuntimeException(e);
-		}
+		prefix = 'b';
+		count = 1;
 	}
 
 	@Override
 	public String generate() {
-		md.reset();
-		md.update(Double.toHexString(Math.random()).getBytes());
-		byte[] digest = md.digest();
-
-		StringBuilder name = new StringBuilder(6);
-		name.append('s');
-		for (int i = 0; i < 5; i++) {
-			int val = (digest[i] & 0xff) + 0x100;
-			name.append(Integer.toString(val, 16).substring(1));
+		if (count == Short.MAX_VALUE) {
+			prefix++;
+			count = 1;
 		}
+		StringBuilder name = new StringBuilder(6);
+		name.append(prefix);
+		name.append(String.format("%05d", count++));
 		return name.toString();
 	}
 
